6256255e5ba751f5b20ff71405b6f8174efa9a1e
Casper Schipper <casper.schipper@gmail.com>
2025-02-13 11:49:46 +0100

    mongodb pipeline with pages

--- db/run.sh
+++ db/run.sh
@@ -1,1 +1,20 @@
-python3 rcData_API_live.py
+#!/bin/bash
+
+# Define the script to run
+SCRIPT="rcData_API_live.py"
+
+# Run the script in the background
+python3 "$SCRIPT" &
+
+# Get the PID of the running script
+PID=$!
+
+# Wait a moment to ensure the process is running
+sleep 1
+
+# Apply CPU limit (25%) to the specific process
+cpulimit -l 25 -p $PID &
+
+# Wait for the script to finish
+wait $PID
+
